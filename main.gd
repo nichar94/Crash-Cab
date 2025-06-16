be0844596f7e331ebb55605d88061645132a4194
nichar94 <nichar94@kingshigh.school.nz>
2025-06-16 12:24:56 +1200

    Timer works, death screen appears

--- main.gd
+++ main.gd
@@ -7,7 +7,7 @@ extends Node2D
 # Variable to track remaining time
 var time_left = 60
 
-func _ready_timeout():
+func _ready():
 	# Connect the timer signal if not connected in editor
 	if not timer.is_connected("timeout", _on_timer_timeout):
 		timer.timeout.connect(_on_timer_timeout)
@@ -25,11 +25,12 @@ func _process(_delta):
 func _on_timer_timeout():
 	# Called every second when timer ticks
 	time_left -= 1
-	update_timer_label()
 	# Check if time is up
 	if time_left <= 0:
 		timer.stop()  # Stop timer to prevent negative time
 		get_tree().change_scene_to_file("res://DeathScreen.tscn")
+	else:
+		update_timer_label()  # Only update label if time isn't up
 
 func update_timer_label():
 	# Update the label to show current time
